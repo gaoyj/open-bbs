6e77efa5053449123bc51611d563feb001a40d26
gaoyj <594168788@qq.com>
2016-08-16 23:24:28 +0800

    merge

---
+++ open-bbs.persistence/src/main/java/com/openbbs/exception/RowMismatchException.java
@@ -0,0 +1,20 @@
+package com.openbbs.exception;
+
+public class RowMismatchException extends Exception {
+
+	public RowMismatchException(){
+		super();
+	}
+	
+	public RowMismatchException(String msg) {
+		super(msg);
+	}
+	
+	public RowMismatchException(Throwable cause) {
+		super(cause);
+	}
+	
+	public RowMismatchException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
